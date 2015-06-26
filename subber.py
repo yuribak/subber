93a1a3903aa7bf56765f619d8107d9a5abe1b192
Yuri Bakulin <yuri.bakulin@gmail.com>
2015-06-26 00:32:31 -0400

    added folder traversal and basic filtering and existance check

--- subber.py
+++ subber.py
@@ -24,6 +24,11 @@ class SubDB(object):
             data += fin.read(self.hash_read_size)
         return hashlib.md5(data).hexdigest()
 
+    def get_sub_fn(self, file):
+        folder, fn = os.path.split(file)
+        fn = fn[:-3] + 'srt'
+        return os.path.join(folder, fn)
+
     def get_sub(self, vid_file_path):
 
         params = {
@@ -32,15 +37,29 @@ class SubDB(object):
 
         subs = requests.get(self.url.format(**params), headers=self.headers)
         if subs.status_code == 200:
-            folder, fn = os.path.split(vid_file_path)
-            fn = fn[:-3] + 'srt'
-            with open(os.path.join(folder, fn), 'wb') as fout:
+
+            with open(self.get_sub_fn(vid_file_path), 'wb') as fout:
                 fout.write(subs.content)
         return subs.status_code == 200
 
+#test_file = '/home/yurib/video/12.Monkeys.S01E03.HDTV.x264-KILLERS.mp4'
 
-subber = SubDB()
-test_file = '/home/yurib/video/12.Monkeys.S01E03.HDTV.x264-KILLERS.mp4'
-print subber.hash(test_file)
-print subber.get_sub(test_file)
-#for root,folders,files in os.walk(root_dir):
+if __name__ == '__main__':
+    subber = SubDB()
+    for root, folders, files in os.walk(root_dir):
+        for fn in files:
+            if fn[-3:] in ['mp4', 'avi', 'mkv']:
+                if 'sample' in root.lower():
+                    result = 'SKIPPING'
+                else:
+                    subfn = subber.get_sub_fn(os.path.join(root,fn))
+                    subfn_missing = subfn + '.missing'
+                    if os.path.isfile(subfn) or os.path.isfile(subfn_missing):
+                        result = 'EXISTS'
+                    else:
+                        got_subs = subber.get_sub(os.path.join(root, fn))
+                        if not got_subs:
+                            with open(subfn_missing,'w') as fout:
+                                fout.write('')
+                        result = 'DOWNLOADD' if got_subs else 'NOT_FOUND'
+                print '{:<96}{}'.format(fn, result)
